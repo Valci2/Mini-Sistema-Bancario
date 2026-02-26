7ecc52994681e27e7f8420b1bc4026f5e2d41f4f
Valci <salin00231@gmail.com>
2026-02-26 19:02:55 -0300

    feito a transação e melhorias no codigo

--- src/main/java/junior/mini_sistema_bancario/repository/ContaRepository.java
+++ src/main/java/junior/mini_sistema_bancario/repository/ContaRepository.java
@@ -3,9 +3,6 @@ package junior.mini_sistema_bancario.repository;
 import junior.mini_sistema_bancario.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ContaRepository extends JpaRepository<Conta, Long> {
-    Optional<Conta> findByEmail(String email);
     boolean existsByEmail(String email);
 }
--- src/test/java/junior/mini_sistema_bancario/MiniSistemaBancarioApplicationTests.java
+++ src/test/java/junior/mini_sistema_bancario/MiniSistemaBancarioApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MiniSistemaBancarioApplicationTests {
 
     @Test
-    void contextLoads() {
+    void TestCreate() {
     }
-
 }
